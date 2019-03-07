3270ca5e1ec2279ff7531a554174f0009cca5c84
Long Dang <frog.dev.studio@gmail.com>
2019-03-07 15:13:42 +0700

    Update

--- pages.js
+++ pages.js
@@ -1,16 +1,16 @@
-var VERIFY_TOKEN = "<Your custom verification code here>";
+var VERIFY_TOKEN = "EAADqWjBZB7owBAOOgLIdYkRkC8t3lTHWdT4RmQpc6Jqdp1igj7QTfHAGf1KxeLpb8RKYokWBZADcuZBIcPG3cTLDhx8CTP0DBR63oWpIEKaxAem48e4WEz3SCRDaUiEJe9rHt540TT5Wp2NrF1QnP7JFq5VJiaKMQZAhi7gzgS5f3kBMg5t7";
 var PAGES = [
 	// Add your pages here, change key for each one, I use numberical values starting at 0
 	{
-		id: "<Your page ID>",
-		name: "<Game Name>",
+		id: "297646080934977",
+		name: "Merge Spaceship IDLE",
 		key: "0",
 		title: "Don't forget your daily free coins",
 		subtitle: "We miss you, come back and play",
 		cta: "Play Now",
 		imageurl: "<URL of image to show in message>",
 		payload: null,
-		pat: "<Enter tour Page Access Token here>",
+		pat: "EAADqWjBZB7owBAOOgLIdYkRkC8t3lTHWdT4RmQpc6Jqdp1igj7QTfHAGf1KxeLpb8RKYokWBZADcuZBIcPG3cTLDhx8CTP0DBR63oWpIEKaxAem48e4WEz3SCRDaUiEJe9rHt540TT5Wp2NrF1QnP7JFq5VJiaKMQZAhi7gzgS5f3kBMg5t7",
 	},
 ];
 
